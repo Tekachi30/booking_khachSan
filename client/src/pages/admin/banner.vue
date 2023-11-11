5ae7c30b7a769bdf80ef2c9fb452e4cec06fd825
Tekachi30 <dangthanhdat72@gmail.com>
2023-11-11 10:08:27 +0700

    delete

--- client/src/pages/admin/banner.vue
+++ client/src/pages/admin/banner.vue
@@ -75,7 +75,7 @@
                                         </li>
 
                                         <li>
-                                            <button @click="deleteBanner()" type="button"
+                                            <button @click="deleteBanner();selectBanner(banner)" type="button"
                                                 class="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 text-red-500 dark:hover:text-red-400">
                                                 <svg class="w-4 h-4 mr-2" viewbox="0 0 14 15" fill="none"
                                                     xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
@@ -222,7 +222,7 @@
     <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
         <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-            <button type="button"
+            <button @click="deleteBanner()" type="button"
                 class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                 data-modal-toggle="deleteModal">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
@@ -231,7 +231,7 @@
                         d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                         clip-rule="evenodd" />
                 </svg>
-                <span class="sr-only">Close modal</span>
+                Xóa
             </button >
             <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true"
                 fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
