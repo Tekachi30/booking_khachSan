9afa01b0e63ccd38fd4b508338619582c146f4eb
Minh Nhựt Nguyễn <nguyen.nhut.99.2017@gmail.com>
2023-11-13 22:46:08 +0700

    push

---
+++ backend/seeders/20231113125048-seed-event.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    // admin => chỉ tạo ra một admin quản lý tất cả
+    // admin admin
+    await queryInterface.bulkInsert('admins', [{
+      account: 'admin2',
+      password: '$2b$10$rtyEsgLL05JW5h8tGsFngOcNZk7eMjfAjfmT6aK6ngK.4GHkejgp6'
+    }], {});
+
+    // owners
+    await queryInterface.bulkInsert('owners', [{
+      account: "owner1",
+      fullname: "owner1 fullname",
+      address: "owner1 address",
+      phone: "owner1 phone",
+      password: "$2b$10$PfsusDUBx6fsGWSl3Kph7e7dzf1JlwhsZ5LyJMXKGSgiljjD.G4ZO",
+      email: "owner1@gmail.com"
+    },
+    {
+      account: "owner2",
+      fullname: "owner2 fullname",
+      address: "owner2 address",
+      phone: "owner2 phone",
+      password: "$2b$10$PfsusDUBx6fsGWSl3Kph7e7dzf1JlwhsZ5LyJMXKGSgiljjD.G4ZO",
+      email: "owner2@gmail.com"
+    },
+    ], {});
+   
+  },
+
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
